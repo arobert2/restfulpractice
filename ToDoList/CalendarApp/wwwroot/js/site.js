6aad68a693a8b40c5837c241d41e985d25712dba
Allen Roberts <allen.roberts1985@gmail.com>
2019-03-04 16:54:09 -0500

    ajax query for ScheduleNewEvent

--- ToDoList/CalendarApp/wwwroot/js/site.js
+++ ToDoList/CalendarApp/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$('.new-event-button').on('click', function() {
+    $.get("../Event/ScheduleNewEvent", function (data) {
+        $(".schedule-event-pane").html(data);
+    });
+});
